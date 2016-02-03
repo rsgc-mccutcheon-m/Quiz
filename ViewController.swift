b7c6a3c4e665bd402c1ef33bd9b871bc5dc845a6
Mark McCutcheon <mmccutcheon@rsgc.on.ca>
2016-02-03 09:51:18 -0500

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Mark on 2016-01-28.
+//  Copyright © 2016 Clutch Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        
+    }
+    
+
+}
+
